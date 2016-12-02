f6fb24ed0c9a2251b9d24ea00be75d467895d22a
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-12-02 15:07:12 +0700

    change port to 8080 (development)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let path = require('path');
 let app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const publicPath = express.static(path.join(__dirname, ''))
 app.use('/', publicPath);
 
